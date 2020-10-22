65f0eec1144faf131804eb599a8d4c9dded2dbb3
saeed-tlb <saeedt7879s@gmail.com>
2020-10-22 20:47:12 +0330

    create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react"
+import { Link } from "gatsby"
+
+// STYLED-COMPONENT
+import { HeaderNav, Logo, Menu } from "../styles/headerStyles"
+import { Container, Flex } from "../styles/globalStyles"
+
+// Context
+import {
+  useGlobalStateContext,
+  useGlobalDispatchContext,
+} from "../context/globalContext"
+import { toggle_theme } from "../actions/action"
+
+const Header = () => {
+  const { currentTheme } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", currentTheme)
+  }, currentTheme)
+
+  const toggleTheme = () => dispatch(toggle_theme(currentTheme))
+
+  return (
+    <HeaderNav
+      initial={{ y: -72, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1, ease: [0.6, 0.05, -0.01, 0.9] }}
+    >
+      <Container>
+        <Flex spaceBetween noHeight>
+          <Logo>
+            <Link to="/">FURR</Link>
+            <span onClick={toggleTheme}></span>
+            <Link to="/">W</Link>
+          </Logo>
+          <Menu>
+            <button>
+              <span></span>
+              <span></span>
+            </button>
+          </Menu>
+        </Flex>
+      </Container>
+    </HeaderNav>
+  )
+}
+
+export default Header
