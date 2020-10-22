41e511c978bd84402f44f821668c162feced1480
saeed-tlb <saeedt7879s@gmail.com>
2020-10-22 20:52:51 +0330

    since we use styled-component in this project we use .js files instead of .css so here we create a file which contains global styles like container, flex and another file which is for header component

---
+++ src/styles/headerStyles.js
@@ -0,0 +1,48 @@
+import style from "styled-components"
+import { motion } from "framer-motion"
+
+export const HeaderNav = style(motion.div)`
+    height: 0px;
+    width: 100%;
+    position: absolute;
+    top: 72px;
+    right: 0;
+    left: 0;
+    z-index: 99;
+`
+
+export const HeaderNav = style.div`
+    a {
+        font-size: 1.8rem;
+        font-weight: 800;
+        color: ${props => props.theme.text}
+    }
+    span {
+        height: 1rem;
+        width: 1rem;
+        background: ${props => props.theme.red};
+        margin: 0 4px;
+        border-radius: 100%;
+        display: inline-block;
+        position: relative;
+        bottom: 2px;
+    }
+`
+
+export const HeaderNav = style.div`
+    button {
+        transform-origin: center;
+        border: none;
+        padding: 20px;
+        background: none;
+        outline: none;
+        
+        span {
+            width: 36px;
+            height: 8px;
+            display: block;
+            background: ${props => props.theme.text};
+            margin: 8px;
+        }
+    }
+`
