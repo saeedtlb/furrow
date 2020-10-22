3022fcc534a3efcdc32e2efc6f4e50c27e82225a
saeed-tlb <saeedt7879s@gmail.com>
2020-10-22 20:50:15 +0330

    create a context and use cutome hooke to use state and dispatch in other component easier

---
+++ src/context/globalContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useReducer, useContext } from "react"
+
+// REDUCER
+import { globalReducer } from "../Reducer/reducer"
+
+// Define the context
+const GlobalStateContext = createContext()
+const GlobalDispatchContext = createContext()
+
+export const GlobalProvider = ({ children }) => {
+  const theme = window.localStorage.getItem("theme")
+  const [state, dispatch] = useReducer(globalReducer, {
+    currentTheme: theme ? theme : "dark",
+  })
+
+  return (
+    <GlobalDispatchContext.Provider value={dispatch}>
+      <GlobalStateContext.Provider value={state}>
+        {children}
+      </GlobalStateContext.Provider>
+    </GlobalDispatchContext.Provider>
+  )
+}
+
+// Custom hooks to use dispatch and state
+export const useGlobalStateContext = () => useContext(GlobalStateContext)
+
+export const useGlobalDispatchContext = () => useContext(GlobalDispatchContext)
