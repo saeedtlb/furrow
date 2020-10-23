fe51ef3686a3ad42dde4ff4ea06317655c7c4fcd
saeed-tlb <saeedt7879s@gmail.com>
2020-10-23 13:39:16 +0330

    create a file for cutome cursor and it's own functionality to work

---
+++ src/components/CustomCursor.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react"
+
+// STYLED-COMPONENT
+import { Customursor } from "../styles/globalStyles"
+
+// CONTEXT
+import { useGlobalStateContext } from "../context/globalContext"
+
+const Cursor = () => {
+  const { cursorType } = useGlobalStateContext()
+  const [mousePosition, setMousePosition] = useState({
+    x: 400,
+    y: 400,
+  })
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove)
+
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove)
+    }
+  }, [])
+
+  const onMouseMove = e => {
+    const { pageX: x, pageY: y } = e
+
+    setMousePosition({ x, y })
+  }
+
+  return (
+    <>
+      <Customursor
+        className={`${cursorType ? "hovered" : ""} ${cursorType}`}
+        style={{
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y}px`,
+        }}
+      />
+    </>
+  )
+}
+
+export default Cursor
