b6ab30d07babe99faad8265c383571231b77ec7d
saeed-tlb <saeedt7879s@gmail.com>
2020-10-27 17:26:30 +0330

    change the file name and add animate_property varriable

---
+++ src/components/utils/misc.js
@@ -0,0 +1,44 @@
+export const navRoutes = [
+  {
+    id: 0,
+    title: "NOT HUMBLE",
+    path: "not_humble",
+    video: "featured-video.mp4",
+  },
+  {
+    id: 1,
+    title: "bleeping easy",
+    path: "bleeping_easy",
+    video: "easy.mp4",
+  },
+  {
+    id: 2,
+    title: "make it zero",
+    path: "make-it-zero",
+    video: "make-it-zero.mp4",
+  },
+  {
+    id: 3,
+    title: "it takes an island",
+    path: "it-takes-an-island",
+    video: "it-takes-an-island.mp4",
+  },
+  {
+    id: 4,
+    title: "50 beaches",
+    path: "50-beaches",
+    video: "50-beaches.mp4",
+  },
+];
+
+export const Animate_property = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6, ease: [0.6, 0.05, -0.01, 0.9] },
+  },
+  hidden: {
+    opacity: 0,
+    y: 72,
+  },
+};
