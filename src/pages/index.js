6815f158b572c992fc0b102163540c694ba2ecb6
saeed-tlb <saeedt7879s@gmail.com>
2020-10-27 13:11:30 +0330

    include Homes component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,34 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
 
 // COMPONENTS
-import HomeBanner from "../components/homePage/HomeBanner"
+import HomeBanner from "../components/homePage/HomeBanner";
+import HomeContent from "../components/homePage/HomeContent";
+import HomeFeatured from "../components/homePage/HomeFeatured";
 
 // Context
 import {
   useGlobalStateContext,
   useGlobalDispatchContext,
-} from "../context/globalContext"
-import { cursor_style } from "../actions/action"
+} from "../context/globalContext";
+import { cursor_style } from "../actions/action";
 
 const IndexPage = () => {
-  const { cursorStyles } = useGlobalStateContext()
-  const dispatch = useGlobalDispatchContext()
+  const { cursorStyles } = useGlobalStateContext();
+  const dispatch = useGlobalDispatchContext();
 
   const onCursor = type => {
-    type = cursorStyles.includes(type) && type
-    dispatch(cursor_style(type))
-  }
+    type = cursorStyles.includes(type) && type;
+    dispatch(cursor_style(type));
+  };
 
   return (
     <Layout>
       <HomeBanner onCursor={onCursor} />
+      <HomeContent />
+      {/* <HomeFeatured /> */}
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
