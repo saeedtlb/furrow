581b3f0fb89d47c17d81af1f82e71bd5de310d96
saeed-tlb <saeedt7879s@gmail.com>
2020-10-28 13:47:30 +0330

    keep accordion texts(data) in a varriable

--- src/components/utils/misc.js
+++ src/components/utils/misc.js
@@ -42,3 +42,59 @@ export const Animate_property = {
     y: 72,
   },
 };
+
+export const accordionIds = [
+  {
+    id: 0,
+    title: "Pre-Production",
+    results: [
+      "Creative Development",
+      "Writing",
+      "Creative Development",
+      "Writing",
+      "Storyboards",
+      "Art Direction",
+      "Creative Direction",
+      "Location Scouting",
+      "Casting",
+    ],
+  },
+  {
+    id: 1,
+    title: "Video Production",
+    results: [
+      "Principle Photography",
+      "Production Management",
+      "Crew",
+      "Dailies",
+      "LTO-Archiving",
+    ],
+  },
+  {
+    id: 2,
+    title: "Post-Production",
+    results: [
+      "Colour correction",
+      "Offline editing",
+      "Online editing",
+      "VFX",
+      "Animation and motion graphics",
+      "Closed captioning and subtitles",
+      "Descriptive video",
+      "Dailies",
+      "Quality control",
+      "LTO Archiving",
+    ],
+  },
+  {
+    id: 3,
+    title: "Audio Post-Production",
+    results: [
+      "We work with some amazing partners who provide:",
+      "Sound Design",
+      "SFX",
+      "Music",
+      "Sound Mix",
+    ],
+  },
+];
